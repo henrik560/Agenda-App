228bda7f06736e63f45a42a66f8c89404e8fa8ff
hhannewijk <henrikh2004@hotmail.com>
2021-09-07 09:15:53 +0200

    .

---
+++ resources/views/Base_Layout/layout.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- <meta name="og:description" content="De beste oplossing in ruwvoermachines!" />
+    <meta name="og:title" content="Budding - Ruwvoermachines" />
+    <meta name="og:url" content="BuddingTechniek.nl" />
+    <meta name="og:description" content="Verkoop van ruwvoermachines." />
+    <meta name="og:image" content="https://i.ibb.co/YWLpr1D/image-removebg-preview.png" />
+    <meta name="og:image:width" content="2048" />
+    <meta name="og:image:height" content="1146" />
+    <meta property="locale" content="nl_NL" />
+    <meta property="og:site_name" content="" /> TODo ->> --}}
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://kit.fontawesome.com/7ea4a14f9b.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;500;700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" />
+    <script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+    <script crossorigin src="https://unpkg.com/react@17/umd/react.production.min.js"></script>
+    <script crossorigin src="https://unpkg.com/react-dom@17/umd/react-dom.production.min.js"></script>
+    @stack('script')
+    @stack('styles')
+</head>
+<body>
+    @yield('content')
+</body>
+    @stack('script')
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('Base_Layout/layout')
+@push('styles')
+    <link href="{{ asset('css/index.css') }}" rel="stylesheet">
+@endpush
+@section('content')
+
+@endsection
