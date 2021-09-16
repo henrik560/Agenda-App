5d737d21449446f370acf3834625a8df67a9273b
hhannewijk <henrikh2004@hotmail.com>
2021-09-16 13:14:17 +0200

    .

--- resources/js/Components/agenda/header/render.js
+++ resources/js/Components/agenda/header/render.js
@@ -14,30 +14,19 @@ export default class header extends React.Component {
     fetchBuildings = async () => {
         await axios.get('api/agenda/buildings/get').then(response => this.setState({buildings: response.data}));
         var buildings = [];
+        var buildingsElement = [];
         Object.values(this.state.buildings).flat().map((el, id) => buildings.push(el))
         this.setState({buildings})
-        console.log(this.state.buildings)
+        buildings.map((element, index) => {
+            
+        })
     }
-
-
 
     render() {
         return(
             <div className="buildings-list-wrapper">
-                {/* { this.state.buildings.forEach(element => {
-                    return (
-                        <div key={element["id"]}>
-                            <div className="buildings-list-item-header">
-                                <span>{element["name"]}</span>
-                            </div>   
-                            <div className="rooms-list-item-">
-                            
-                            </div>
-                        </div>
-                    )
-                })} */}
             </div>
         )
-    }
+    }   
 
 }
