0f920f0e4832c59586dd8510276dd9bde06059b3
hhannewijk <git-bot@inventivetalent.org>
2021-10-05 23:14:49 +0200

    /

--- resources/views/agenda/overview.blade.php
+++ resources/views/agenda/overview.blade.php
@@ -3,7 +3,29 @@
         <link rel="stylesheet" type="text/css" href="{{ asset("css/agenda.css") }}">
     @endpush
 @section('content')
-<div id="agenda-wrapper"></div>
+{{-- <div id="agenda-wrapper"></div> --}}
+
+<div class="agenda-container">
+    <div class="agenda-child-container">
+        <div class="header-container">
+            <div class="sticky-header">
+            </div>
+        </div>
+        <div class="content-container">
+            <a>test</a>
+            <a>test</a>
+            <a>test</a>
+            <a>test</a>
+            <a>test</a>
+            <a>test</a>
+            <a>test</a>
+            <a>test</a>
+            <a>test</a>
+            <a>test</a>
+            <a>test</a>
+        </div>
+    </div>
+</div>
 @endsection
     @push('script')
         <script src={{ asset("js/datatables/agenda/agenda.js") }}></script>
